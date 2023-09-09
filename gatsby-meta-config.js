46779b2031b0a9d965cbf0ab203aa70f2c6620b9
sylee <sylee6529@gmail.com>
2023-09-09 16:29:40 +0900

    ✨ feat: about page 내용 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `sylee6529`,
+  description: `아무래도 개발`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://sylee6529.gihub.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: `zoomkoding/zoomkoding-gatsby-blog`, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `sylee6529/sylee6529.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
-  ga: 'UA-134826755-2', // Google Analytics Tracking ID
+  ga: 'G-F73PXRC2KH', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `이서연`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'zoomkoding.gif', // Path to the image in the 'asset' folder
+      description: ['이것저것 관심이 많은', '집요함을 가진', '배움의 자세를 가진'],
+      thumbnail: '230909-thumb.gif', // Path to the image in the 'asset' folder
     },
     social: {
-      github: `https://github.com/zoomkoding`,
-      linkedIn: `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: `zoomkoding@gmail.com`,
+      github: `https://github.com/sylee6529`,
+      linkedIn: `https://www.linkedin.com/in/seoyeon-lee-075514206`,
+      email: `sylee6529@gmail.com`,
     },
   },
 
@@ -43,63 +43,47 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2018.04 ~',
-        activity: '개발 시작 & 블로그 시작',
+        date: '2021.07 ~2021.08',
+        activity: 'AI양재허브 AI 실무 개발자 양성 교육 수료',
       },
       {
-        date: '2018.07 ~ 2018.11',
-        activity: '인공지능 육목 알고리즘 개발 & 경진대회 우승',
+        date: '2021.08',
+        activity: 'AI 기반 지하철 혼잡도 제공 웹서비스 개발',
+        links: {
+          demo: 'https://www.codepresso.kr/ai/2',
+        },
+      },
+      {
+        date: '2021.09 ~ 2022.02',
+        activity: '에듀테크 스타트업 웹개발팀 인턴 근무',
+      },
+      {
+        date: '2022.12 ~ 2023.01',
+        activity: '인생을 포트폴리오로, Lifolio 개발',
+        links: {
+          github: 'https://github.com/Lifolio',
+        },
+      },
+      {
+        date: '2023.05 ~ 2023.06',
+        activity: 'MBTI 성격유형 테스트 여행지편 개발',
+        links: {
+          github: 'https://github.com/Team-MBTI/backend',
+        },
+      },
+      {
+        date: '2023.08',
+        activity: '그림 일기를 그려주는 AI 서비스, GGOOMI 개발',
+        links: {
+          github: 'https://github.com/prompter-day-2023',
+        },
       },
 
       {
-        date: '2019.06 ~ 2019.12',
-        activity: '알고리즘 휴학 & 코드포스 블루 달성',
+        date: '2020.06~',
+        activity: '여행 블로그 TRIP IN WOLRD 개발 중',
         links: {
-          post: '/how-to-algo',
-        },
-      },
-      {
-        date: '2019.12 ~ 2020.06',
-        activity: '스타트업 인턴십 진행 & Picky 개발',
-        links: {
-          post: '/start-up-app-development',
-          googlePlay: 'https://play.google.com/store/apps/details?id=care.jivaka.picky',
-          appStore: 'https://apps.apple.com/app/picky-skincare-made-smarter/id1504197356',
-        },
-      },
-
-      {
-        date: '2020.07 ~ 2020.08',
-        activity: '2020 우아한테크캠프 참여',
-        links: {
-          post: '/woowa-tech-camp-final',
-          github: 'https://github.com/woowa-techcamp-2020/bmart-6',
-        },
-      },
-
-      {
-        date: '2021.01 ~ 2021. 04',
-        activity: '대학시간(대학교 수강신청 보조 웹 앱) 개발 및 운영',
-        links: {
-          post: '/college-timetable-development',
-          github: 'https://github.com/zoomkoding/college-timetable',
-          demo: 'https://www.timetable.college',
-        },
-      },
-      {
-        date: '2021.02 ~',
-        activity: '개발 블로그 스타터 개발 및 유지 보수',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
-      {
-        date: '2021.04 ~',
-        activity: '만다오(프로모션 웹앱 빌더) 개발',
-        links: {
-          post: 'https://techblog.woowahan.com/2719',
+          github: 'https://github.com/WAGGLE-TEAM',
         },
       },
     ],
@@ -123,49 +107,26 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '만다오(프로모션 웹앱 빌더) 개발',
+        title: '에듀테크 스타트업 웹개발팀 인턴 근무',
         description:
-          '만다오는 우아한형제들에 입사한 후 처음으로 진행한 프로젝트로, 각종 프로모션 페이지를 마케터와 디자이너가 개발자 없이 만들 수 있게 하기 위한 WYSIWYG 방식의 웹 에디터입니다. 만다오 팀은 모든 사람이 디자인을 해야한다는 신념 하에 모든 팀원들이 직접 기획, 디자인, 개발을 진행하고 있습니다. 만다오는 제가 합류한 후에 고도화된 기능들이 많이 추가되어 가고 있고, 최종적으로는 모든 프로모션 페이지를 만들 수 있도록 계속해서 기능을 확장해나갈 예정입니다.',
-        techStack: ['react', 'nestjs'],
-        thumbnailUrl: 'mandao.png',
-        links: {
-          post: 'https://techblog.woowahan.com/2719',
-        },
+          '주 업무는 Amazon Rekognition에서 받아온 json 데이터를 기반하여 동영상 처리 관련 로직을 작성하고, 사내Admin 페이지 개발하였고, 외부 사업(외주) 백엔드 개발을 맡았습니다. Agile 개발 프로세스 중 하나인 Scrum 방식으로 개발을 진행하며, 백로그를 적절히 정하고 회고를 통해 매 주 팀 모두가 개선할 수 있도록 노력하는 과정에 익숙해질 수 있었습니다. 그리고 처음 접하는 언어와 프레임워크여도, 단기간에 학습하고 적용하는 능력을 키울 수 있었습니다. 또한, 외부사업 개발을 맡으면서, 클라이언트와 요구 사항을 정리하고 최초의 프로젝트를 설계하는 과정부터 참여하며 실제 프로덕트가 개발되는 과정과 구조, 팀원과 협업하는 방법에 대한 이해도를 높일 수 있었습니다.',
+        techStack: ['FastAPI', 'Vue.js', 'Typescript', 'Python'],
+        thumbnailUrl: 'intern_stack.png',
       },
       {
-        title: 'Picky(글로벌 스킨케어 제품 분석 모바일 앱) 개발',
+        title: 'MBTI 성격유형테스트 여행지편 개발',
         description:
-          '알고리즘을 공부하기 위해 휴학을 하고 있던 중에 실무를 경험해보고자 구글 출신 선배가 창업한 스타트업에서 인턴십을 진행하게 되었습니다. 제가 팀에 합류한 후 회사가 기존 진행하던 사업에서 앱 사업으로 피버팅을 하게 되었고, 저는 홀로 개발을 맡아 새로운 앱 프로젝트 참여하게 되었습니다. 모르는 것들을 공부하고 주변에 물어보면서 두 달 가까이 되는 기간동안 열심히 개발을 진행하여, Picky라는 글로벌 앱 서비스를 배포하게 되었습니다. 빠르게 배포한 후에 유저들의 피드백을 적극 수용하며 지속적으로 앱을 발전시켜 나갔고, 점차 서비스 이용자가 늘어가는 것을 경험했습니다. 그렇게 약 4개월 넘게 유지보수를 진행하던 중에 학업을 위해 팀을 떠나게 되었습니다. 더 오랜 기간 함께 하지 못해 아쉽지만 스타트업 서비스의 첫 시작부터 여러 서비스의 변곡점을 경험하면서 개발적으로도 많이 성장할 수 있었을 뿐만 아니라 서비스 성장을 직접 목격하면서 시야를 넓힐 수 있었던 매우 값진 시간이었습니다.',
-        techStack: ['flutter', 'nodejs'],
-        thumbnailUrl: 'picky.png',
-        links: {
-          post: '/start-up-app-development',
-          googlePlay: 'https://play.google.com/store/apps/details?id=care.jivaka.picky',
-          appStore: 'https://apps.apple.com/app/picky-skincare-made-smarter/id1504197356',
-        },
+          '지정된 12가지의 문항의 선택지를 선택하면, MBTI 성격 분류에 따라 여행지와 여행 스타일 결과를 알려주는 성격 유형 테스트 개발을 진행하였습니다. 4년차 현직자이신 백엔드 팀원분과 함께 하면서, VPC 피어링, Docker compose 사용법 등 인프라와 CI/CD에 대한 것을 새롭게 알게 되었고 헥사고날 아키텍처와 DDD 구현을 하며 아키텍처에 대한 공부와 고민을 할 수 있었던 값진 시간이었습니다.',
+        techStack: ['NextJS', 'typescript', 'Naver Cloud', 'Docker'],
+        thumbnailUrl: 'mbti_test.png',
       },
       {
-        title: '개발 블로그 테마 개발',
+        title: '그림일기를 그려주는 AI 서비스, 꾸미(GGOOMI) 개발',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+          'SK텔레콤과 OpenAI가 개최한 Generative AI 해커톤인 Prompter Day Seoul 2023 참가를 위해 개발한 서비스입니다. GPT turbo 3.5, Dall-E, Deepl API를 사용하여 일기 내용을 요약하고 텍스트를 추출하여 그림일기를 생성한 후, 라인드로잉으로 변환하여 최종적으로 컬러링 도안을 저장하고 출력할 수 있는 결과물을 제공합니다. 백엔드 개발과 프롬프팅 개선에 주로 기여하였습니다. 2주도 안 되는 짧은 시간동안 아이디어 기획부터 MVC 구현까지 무사히 마쳤습니다.',
+        techStack: ['Flask', 'Docker', 'GitHub Action', 'Prompting'],
+        thumbnailUrl: 'ggoomi_draw.jpg',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
-      },
-      {
-        title: '대학시간(대학교 수강신청 보조 웹 앱) 개발',
-        description:
-          '학생들이 예비 수강 신청 과정에서 겪게 되는 불편함을 개선하고자 예비 수강 신청을 도와줄 수 있는 웹사이트를 만들게 되었습니다. 특히 학기가 시작되기 전 빈번하게 바뀌는 과목 정보들을 주기적으로 최신화하고 수강 신청을 시작한 후에는 수강 공석(이삭)을 발견하여 이를 이메일로 알려주는 서비스를 진행했습니다. 2021년 1학기부터 운영하고 있으며 매학기 1000명이 넘는 재학생이 사용하고 있습니다.',
-        techStack: ['react', 'nodejs'],
-        thumbnailUrl: 'timetable.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
         },
       },
     ],
